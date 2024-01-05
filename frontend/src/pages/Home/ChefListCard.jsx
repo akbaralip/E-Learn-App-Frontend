ca281cd9a4bf02b615c81956b7a42e1dbaecb44e
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2024-01-05 20:17:28 +0530

    chef card show the only 4 item

--- frontend/src/pages/Home/ChefListCard.jsx
+++ frontend/src/pages/Home/ChefListCard.jsx
@@ -1,49 +1,49 @@
-
+import React from "react";
 import {
-    Card as MaterialUICard,
-    CardActionArea,
-    CardContent,
-    CardMedia,
-    Typography,
-  } from "@mui/material";
-  import React from "react";
-  import { baseUrl } from "../../Redux/Store/baseUrl/BaseUrl";
-  import './Card.css'
-  
-  function ChefListCard({ chefs }) {
-    if (!chefs || chefs.length === 0) {
-      return null;
-    }
-  
-    return (
-      <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {chefs.map((chef) => (
-          <div key={chef.id} >
-            <MaterialUICard >
-              <CardActionArea>
-                <CardMedia
-                  className="custom-media"
-                  component="img"
-                  image={`${baseUrl}${chef.image_url}`}
-                  alt="Chef Image"
-                />
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant="body1"
-                    component="div"
-                    className="custom-heading flex justify-center items-center"
-                  >
-                    {chef.name}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </MaterialUICard>
-          </div>
-        ))}
-      </div>
-    );
+  Card as MaterialUICard,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { baseUrl } from "../../Redux/Store/baseUrl/BaseUrl";
+import "./Card.css";
+
+function ChefListCard({ chefs }) {
+  if (!chefs || chefs.length === 0) {
+    return null;
   }
-  
-  export default ChefListCard;
-  
+
+  const displayedChefs = chefs.slice(0, 4);
+
+  return (
+    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {displayedChefs.map((chef) => (
+        <div key={chef.id}>
+          <MaterialUICard>
+            <CardActionArea>
+              <CardMedia
+                className="custom-media"
+                component="img"
+                image={`${baseUrl}${chef.image_url}`}
+                alt="Chef Image"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="body1"
+                  component="div"
+                  className="custom-heading flex justify-center items-center"
+                >
+                  {chef.name}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </MaterialUICard>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ChefListCard;
