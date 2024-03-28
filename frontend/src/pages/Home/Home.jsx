4080ab41a324719832a90c63acb19821337120c9
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2024-03-28 10:22:56 +0530

    video autoplay added

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -201,7 +201,7 @@ function Home() {
           <iframe
             width="100%"
             height="315"
-            src="https://www.youtube.com/embed/I_6aMZwf9CU"
+            src="https://www.youtube.com/embed/I_6aMZwf9CU?autoplay=1"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
